1760c861d1b90b15cead838b2422105b5d4e2de5
pedrotenorio05 <pedrohtenorioufrpe@gmail.com>
2025-11-10 19:46:23 -0300

    reorganizacao das pastas do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Hero from "@/components/Hero";
+import Hero from "@/components/home/Hero";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <Hero />
     </main>
   );
