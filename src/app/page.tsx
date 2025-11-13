3c739772f9403aec8fd4b78e250ad608c7818b02
Hivison <84513910+FuscaRetratil@users.noreply.github.com>
2025-11-13 17:29:14 +0000

    fix: import form page correct build ci errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/Hero";
-import ServicesSection from "@/components/sections/ServicesSection";
+import Hero from "../components/home/Hero";
+import ServicesSection from "../components/sections/ServicesSection";
 
 export default function Home() {
   return (
