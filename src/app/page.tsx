91c70931ef50e9f5317642d7a77d1aa88d3b384d
ocaraquetemata <ocaraquetemata@gmail.com>
2025-11-13 14:21:00 +0000

    Section About Us created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/sections/Hero";
+import AboutUs from "@/components/sections/AboutUs";
 import ServicesSection from "@/components/sections/ServicesSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <AboutUs />
       <ServicesSection />
     </main>
   );
